6770b67270d7c4e966d4e6c3c76e2004eaa2c5b0
Roberto P. Villalva <RoVi@MBP.local>
2015-06-25 19:06:25 -0700

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
 
-root 'home#index'
+  root 'home#index'
 
-resources :home, only: [:index]
+  resources :home, only: [:index]
 
-resources :about, only: [:index]
+  resources :about, only: [:index]
 
-resources :posts do
-  resources :comments, only: [:create, :destroy]
-end
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :users, except: [:edit, :update] do
+    get :edit, on: :collection
+    patch :update, on: :collection
+
+  end
+
+  resources :sessions, only: [:new, :create] do
+      delete :destroy, on: :collection
+  end
 
 end
